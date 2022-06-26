5e99e70390ae3a14b6814c8ee9360cd14b971286
leoesttevam <leonardoestevam17@gmail.com>
2022-06-25 21:13:21 -0300

    Java

---
+++ Java/src/tratamento/Program.java
@@ -0,0 +1,27 @@
+package tratamento;
+
+import java.util.InputMismatchException;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		try {
+			String[] vect = sc.nextLine().split(" ");
+			int pos = sc.nextInt();
+			System.out.println(vect[pos]);
+		}
+		catch (ArrayIndexOutOfBoundsException e) {
+			System.out.println("Invalid position!");
+		}
+		catch (InputMismatchException e) {
+			System.out.println("Input error");
+		}
+		sc.close();
+	}
+}
